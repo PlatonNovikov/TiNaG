dbab65d7b0995739a2ce48a98944e401ece93652
PlatonNovikov <121054576+PlatonNovikov@users.noreply.github.com>
2024-11-12 17:38:18 +0100

    Update generator.hpp

--- generator.hpp
+++ generator.hpp
@@ -4,8 +4,6 @@
 typedef struct {
     int y;
     int x;
-    int y_e;
-    int x_e;
 } Coordinate;
 
 void fillPath(int** MAP_RIVER, int start_x, int start_y, int end_x, int end_y) {
@@ -27,38 +25,74 @@ void fillPath(int** MAP_RIVER, int start_x, int start_y, int end_x, int end_y) {
 }
 
 void GENERATOR_RIVER(int** MAP_RIVER, int width, int height) {
+    Coordinate rand_start;
 
-    int randx = rand() % width;
-    int randy = rand() % height;
+    int begin_side = rand() % 4; //1 - up, 2 - left, 3 - down, 4 - right
 
-    for (int i = 0; i < log2(width * height); i++) {
-        if (MAP_RIVER[randy][randx] != 0) {
-            i++;
-            continue;
-        }
+    switch (begin_side){
+    case 0:
+        rand_start.x = rand() % width;
+        rand_start.y = 0;
+        break;
+    
+    case 1:
+        rand_start.x = 0;
+        rand_start.y = rand() % height;
+        break;
 
-        Coordinate dir[4];
-        int dir_q = 0;
+    case 2:
+        rand_start.x = rand() % width;
+        rand_start.y = height;
+        break;
 
-        // Check all four directions and add valid moves to `dir`
-        if (randy > 0 && MAP_RIVER[randy - 1][randx] == 0) {
-            dir[dir_q++] = (Coordinate){randy - 1, randx, randy - 1 + randint(0, width), randx};
-        }
-        if (randx > 0 && MAP_RIVER[randy][randx - 1] == 0) {
-            dir[dir_q++] = (Coordinate){randy, randx - 1};
-        }
-        if (randy < height - 1 && MAP_RIVER[randy + 1][randx] == 0) {
-            dir[dir_q++] = (Coordinate){randy + 1, randx};
-        }
-        if (randx < width - 1 && MAP_RIVER[randy][randx + 1] == 0) {
-            dir[dir_q++] = (Coordinate){randy, randx + 1};
-        }
+    case 3:
+        rand_start.x = width;
+        rand_start.y = rand() % height;
+        break;
 
-        if (dir_q > 0) {
-            int choice = rand() % dir_q;
-            randy = dir[choice].y;
-            randx = dir[choice].x;
-            MAP_RIVER[randy][randx] = 1; 
-        }
+    default:
+        break;
     }
-}
+    int cont_riv_count = 0;
+    for (int i = 0; i < ceil(log2(height * width)) + 1; i++){
+        Coordinate cont_riv_end[4];
+        cont_riv_count = 0;
+
+        if (rand_start.x > 0 && MAP_RIVER[rand_start.y][rand_start.x - 1] == 0){
+                cont_riv_end[cont_riv_count].x = rand_start.x - rand() % rand_start.x;
+                cont_riv_end[cont_riv_count].y = rand_start.y;
+                cont_riv_count++;
+
+            }
+
+        if (rand_start.x < width - 1 && MAP_RIVER[rand_start.y][rand_start.x + 1] == 0){
+                cont_riv_end[cont_riv_count].x = rand_start.x + rand() % (width - rand_start.x);
+                cont_riv_end[cont_riv_count].y = rand_start.y;
+                cont_riv_count++;
+            }
+
+        if (rand_start.y > 0 && MAP_RIVER[rand_start.y - 1][rand_start.x] == 0){
+                cont_riv_end[cont_riv_count].x = rand_start.x;
+                cont_riv_end[cont_riv_count].y = rand_start.y - rand() % rand_start.y;
+                cont_riv_count++;
+            }
+
+        if (rand_start.y < height - 1 && MAP_RIVER[rand_start.y + 1][rand_start.x] == 0){
+                cont_riv_end[cont_riv_count].x = rand_start.x;
+                cont_riv_end[cont_riv_count].y = rand_start.y + rand() % (height - rand_start.y);
+                cont_riv_count++;
+            }
+
+        if (cont_riv_count == 0){
+            continue;
+            }
+
+        Coordinate rand_end = cont_riv_end[rand() % cont_riv_count];
+
+        printf("Start: %d %d\n", rand_start.x, rand_start.y);
+
+        fillPath(MAP_RIVER, rand_start.x, rand_start.y, rand_end.x, rand_end.y);
+
+        rand_start = rand_end;
+        }
+    }  
